beae25d31b339296f80e7c79a5a6b771b249f5f0
Netesh5 <netesh.paudel@gmail.com>
2025-05-03 13:47:44 +0545

    feat: used go routine and channel to shutdown channel gracefully

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/netesh5/student_crud/internal/config"
@@ -12,7 +18,7 @@ func main() {
 	println(config.Env)
 
 	router := mux.NewRouter()
-
+	router.HandleFunc("/", initialPage).Methods("GET")
 	router.HandleFunc("/students", getStudents).Methods("GET")
 
 	server := http.Server{
@@ -20,13 +26,37 @@ func main() {
 		Handler: router,
 	}
 	println("Server is running on port", config.Server.Address)
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
 
+	done := make(chan os.Signal, 1)
+
+	// Listen for interrupt/termination signals
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "error", err.Error())
+		}
+	}()
+
+	<-done // Block until a signal is received
+	slog.Info("Shutting down server...")
+
+	// Create a context with timeout for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server forced to shutdown", "error", err.Error())
+	} else {
+		slog.Info("Server exited gracefully")
 	}
 
 }
 
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
+}
+
+func initialPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the Student CRUD API"))
 }
